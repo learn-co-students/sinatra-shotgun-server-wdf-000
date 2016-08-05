6ab505a2794a57b59892980adb07dfeffa2b46c2
ozPop <ozziepopovas@gmail.com>
2016-08-05 11:44:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! " +
+    "What happens now? " +
+    "Shotgun is running!!!"
   end
 
 end
